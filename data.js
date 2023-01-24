f0ea4b80cbf217e0852b1a13bf602a7ceac4faf1
Fabiana Hasu <fabi.hasu@gmail.com>
2023-01-24 09:45:44 -0300

    Updates

---
+++ data.js
@@ -0,0 +1,14 @@
+// estas funciones son de ejemplo
+
+export const buscarPersonajesPorCasa = (personajes, searchHouse) => {
+  const todaLaCasa = personajes.filter(function(persona){
+    return persona.house === searchHouse;
+  }).map(function (persona){
+    return persona.name;
+  }); 
+  return todaLaCasa
+};
+
+
+
+
